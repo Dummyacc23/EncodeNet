26cbf8b03867c17649775c2aa70bb8e289f157ad
Dummyacc23 <128007903+Dummyacc23@users.noreply.github.com>
2023-03-25 15:49:19 -0500

    Add files via upload

---
+++ Testing/Knowledge_Distillation_AlexNet_inference.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+# metrics 
+from keras.metrics import categorical_crossentropy
+# optimization method
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD, Adam
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+
+from tensorflow import keras
+
+class Distiller(keras.Model):
+    def __init__(self, student, teacher):
+        super().__init__()
+        self.teacher = teacher
+        self.student = student
+
+    def compile(
+        self,
+        optimizer,
+        metrics,
+        student_loss_fn,
+        distillation_loss_fn,
+        alpha=0.1,
+        temperature=3,
+    ):
+        """ Configure the distiller.
+
+        Args:
+            optimizer: Keras optimizer for the student weights
+            metrics: Keras metrics for evaluation
+            student_loss_fn: Loss function of difference between student
+                predictions and ground-truth
+            distillation_loss_fn: Loss function of difference between soft
+                student predictions and soft teacher predictions
+            alpha: weight to student_loss_fn and 1-alpha to distillation_loss_fn
+            temperature: Temperature for softening probability distributions.
+                Larger temperature gives softer distributions.
+        """
+        super().compile(optimizer=optimizer, metrics=metrics)
+        self.student_loss_fn = student_loss_fn
+        self.distillation_loss_fn = distillation_loss_fn
+        self.alpha = alpha
+        self.temperature = temperature
+
+    def train_step(self, data):
+        # Unpack data
+        x, y = data
+
+        # Forward pass of teacher
+        teacher_predictions = self.teacher(x, training=False)
+
+        with tf.GradientTape() as tape:
+            # Forward pass of student
+            student_predictions = self.student(x, training=True)
+
+            # Compute losses
+            student_loss = self.student_loss_fn(y, student_predictions)
+
+            # Compute scaled distillation loss from https://arxiv.org/abs/1503.02531
+            # The magnitudes of the gradients produced by the soft targets scale
+            # as 1/T^2, multiply them by T^2 when using both hard and soft targets.
+            distillation_loss = (
+                self.distillation_loss_fn(
+                    tf.nn.softmax(teacher_predictions / self.temperature, axis=1),
+                    tf.nn.softmax(student_predictions / self.temperature, axis=1),
+                )
+                * self.temperature**2
+            )
+
+            loss = self.alpha * student_loss + (1 - self.alpha) * distillation_loss
+
+        # Compute gradients
+        trainable_vars = self.student.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        # Update the metrics configured in `compile()`.
+        self.compiled_metrics.update_state(y, student_predictions)
+
+        # Return a dict of performance
+        results = {m.name: m.result() for m in self.metrics}
+        results.update(
+            {"student_loss": student_loss, "distillation_loss": distillation_loss}
+        )
+        return results
+
+    def test_step(self, data):
+        # Unpack the data
+        x, y = data
+
+        # Compute predictions
+        y_prediction = self.student(x, training=False)
+
+        # Calculate the loss
+        student_loss = self.student_loss_fn(y, y_prediction)
+
+        # Update the metrics.
+        self.compiled_metrics.update_state(y, y_prediction)
+
+        # Return a dict of performance
+        results = {m.name: m.result() for m in self.metrics}
+        results.update({"student_loss": student_loss})
+        return results
+    
+    
+AlexNet= tf.keras.models.load_model('AlextNet_Original.h5')
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+cifar_x_train=x_train
+cifar_x_test =x_test
+cifar_y_train = y_train
+cifar_y_test = y_test
+cifar_x_train, cifar_x_test = x_train / 255.0, x_test / 255.0
+ 
+# flatten the label values
+cifar_y_train, cifar_y_test = y_train.flatten(), y_test.flatten()
+
+
+def AlexNet_Pruned():
+        AlexNet = Sequential()
+        AlexNet.add(Conv2D(30,(3,3),strides=(1,1),input_shape=(32,32,3), padding = 'same', activation='relu'))
+        AlexNet.add(MaxPooling2D(pool_size=(3,3),strides=(2,2),padding = 'same'))
+        AlexNet.add(Conv2D(40,(5,5),strides=(1,1),padding='same',activation='relu'))
+        AlexNet.add(MaxPooling2D(pool_size=(3,3),strides=(2,2),padding = 'same'))
+        AlexNet.add(Conv2D(80,(3,3),strides=(1,1),padding='same',activation='relu'))
+        AlexNet.add(Conv2D(80,(3,3),strides=(1,1),padding='same',activation='relu'))
+        AlexNet.add(Conv2D(60,(3,3),strides=(1,1),padding='same',activation='relu'))
+        AlexNet.add(MaxPooling2D(pool_size=(3,3),strides=(2,2),padding ='same'))
+        AlexNet.add(Flatten())
+        AlexNet.add(Dense(256, activation='relu'))
+        AlexNet.add(Dropout(0.5))
+        AlexNet.add(Dense(512, activation='relu'))
+        AlexNet.add(Dropout(0.5))
+        AlexNet.add(Dense(10,activation='softmax'))
+
+        
+
+        return AlexNet
+    
+    
+Student_AlexNet= AlexNet_Pruned()
+
+
+distiller = Distiller(student=Student_AlexNet, teacher=AlexNet)
+distiller.compile(
+    optimizer=keras.optimizers.Adam(),
+    metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    student_loss_fn=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    distillation_loss_fn=keras.losses.KLDivergence(),
+    alpha=0.1,
+    temperature=10,
+)
+distiller.load_weights('knowledge_distillation_AlexNet')
+
+import time
+start = time.time()
+score = distiller.evaluate(cifar_x_test, cifar_y_test, verbose=0)
+end = time.time()
+print("Time Taken by Knowledge Distillation For AlexNet",end-start)
+print('Test loss:', score[1])
+print('Test accuracy:', score[0])
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
